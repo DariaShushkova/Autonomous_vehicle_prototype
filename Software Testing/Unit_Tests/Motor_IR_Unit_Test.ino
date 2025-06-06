97e80b9a9e37b0aaee048a442d7f80abfb5a0fbe
Amer195 <163016879+Amer195@users.noreply.github.com>
2025-06-06 14:50:16 +0200

    Added motors and IR integrated unit tests

---
+++ Software Testing/Unit_Tests/Motor_IR_Unit_Test.ino
@@ -0,0 +1,95 @@
+// Motor pins - same as original
+#define LEFT_MOTOR_SPEED_PIN 10
+#define RIGHT_MOTOR_SPEED_PIN 11
+#define LEFT_MOTOR_FORWARD_PIN 2
+#define LEFT_MOTOR_BACKWARD_PIN 3
+#define RIGHT_MOTOR_FORWARD_PIN 4
+#define RIGHT_MOTOR_BACKWARD_PIN 5
+#define leftIR 13
+#define rightIR 12
+// Minimal effective speeds (L298N needs at least ~60 PWM)
+#define LEFT_SPEED 70  // Slightly higher for weaker left motor
+#define RIGHT_SPEED 70
+
+
+void setup() {
+  // Initialize motor control pins
+  pinMode(LEFT_MOTOR_SPEED_PIN, OUTPUT);
+  pinMode(RIGHT_MOTOR_SPEED_PIN, OUTPUT);
+  pinMode(LEFT_MOTOR_FORWARD_PIN, OUTPUT);
+  pinMode(LEFT_MOTOR_BACKWARD_PIN, OUTPUT);
+  pinMode(RIGHT_MOTOR_FORWARD_PIN, OUTPUT);
+  pinMode(RIGHT_MOTOR_BACKWARD_PIN, OUTPUT);
+  pinMode(leftIR,INPUT);
+  pinMode(rightIR,INPUT);
+  Serial.begin(9600);
+  
+}
+
+void loop() {
+  bool left_IR=digitalRead(leftIR);
+  bool right_IR=digitalRead(rightIR);
+  if(left_IR==HIGH && right_IR==LOW){
+    stopMotors();
+    
+    turnLeft();
+    
+    
+   
+
+  }
+  else if(left_IR==LOW && right_IR==HIGH){
+    stopMotors();
+    
+    turnRight();
+    
+    
+    
+  }
+  else if(left_IR==HIGH && right_IR==HIGH){
+    stopMotors();
+   
+  }
+  else{
+    moveForward();
+    
+  }
+ 
+
+
+  
+}
+
+void moveForward() {
+  analogWrite(LEFT_MOTOR_SPEED_PIN, LEFT_SPEED);
+  analogWrite(RIGHT_MOTOR_SPEED_PIN, RIGHT_SPEED);
+  digitalWrite(LEFT_MOTOR_FORWARD_PIN, LOW);
+  digitalWrite(LEFT_MOTOR_BACKWARD_PIN, HIGH);
+  digitalWrite(RIGHT_MOTOR_FORWARD_PIN, LOW);
+  digitalWrite(RIGHT_MOTOR_BACKWARD_PIN, HIGH);
+}
+void turnRight(){
+   
+  analogWrite(RIGHT_MOTOR_SPEED_PIN, 85);
+  digitalWrite(LEFT_MOTOR_FORWARD_PIN, LOW);
+  digitalWrite(LEFT_MOTOR_BACKWARD_PIN, LOW);
+  digitalWrite(RIGHT_MOTOR_FORWARD_PIN, LOW);
+  digitalWrite(RIGHT_MOTOR_BACKWARD_PIN, HIGH);
+}
+void turnLeft(){
+  analogWrite(LEFT_MOTOR_SPEED_PIN, 85);
+
+  digitalWrite(LEFT_MOTOR_FORWARD_PIN, LOW);
+  digitalWrite(LEFT_MOTOR_BACKWARD_PIN, HIGH);
+  digitalWrite(RIGHT_MOTOR_FORWARD_PIN, LOW);
+  digitalWrite(RIGHT_MOTOR_BACKWARD_PIN, LOW);
+}
+void stopMotors(){
+   analogWrite(LEFT_MOTOR_SPEED_PIN, 255);
+  analogWrite(RIGHT_MOTOR_SPEED_PIN,255);
+  digitalWrite(LEFT_MOTOR_FORWARD_PIN, HIGH);
+  digitalWrite(LEFT_MOTOR_BACKWARD_PIN, HIGH);
+  digitalWrite(RIGHT_MOTOR_FORWARD_PIN, HIGH);
+  digitalWrite(RIGHT_MOTOR_BACKWARD_PIN, HIGH);
+
+}
